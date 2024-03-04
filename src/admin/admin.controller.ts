a31b005d1354edb3dc961606fee812090d54b114
krispamB <chrystopam@gmail.com>
2024-03-04 13:59:02 +0100

    [feat]: add roles guard and decorator

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { JwtGuard, RolesGuard } from 'src/common/Guards';
+import { RolesDecorator } from 'src/common/decorators';
 
+@UseGuards(JwtGuard)
 @Controller('admin')
-export class AdminController {}
+export class AdminController {
+  
+  @RolesDecorator('ADMIN')
+  @UseGuards(RolesGuard)
+  @Get()
+  test() {
+    return 'This is an admin route'
+  }
+}
