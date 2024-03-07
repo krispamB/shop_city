4f9640f5f906eb14ba58d9c6ace06b7bb1b42198
krispamB <chrystopam@gmail.com>
2024-03-07 10:15:32 +0100

    [feat]: add admin get all shops

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
+import { ProfileService } from 'src/profile/profile.service';
 
 @Module({
   controllers: [AdminController],
-  providers: [AdminService],
+  providers: [AdminService, ProfileService],
 })
 export class AdminModule {}
